26367db0a615fd27a94dc27223c89dc1a29a7724
CodeWithSammy <mrsam03102002@gmail.com>
2025-08-14 17:56:51 +0530

    production ready updates and build configurations

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,26 +4,34 @@ import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js"; // Import the rate limiting middleware
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); // Get the current directory name
 
 // Middleware to handle CORS and JSON parsing
 // Allow requests from the frontend and specify allowed methods
-app.use(cors(
-  {
-    origin: "http://localhost:5173", // Allow requests from the frontend
-    methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-  }
-))
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(rateLimiter); // Apply rate limiting middleware
 
 
 app.use("/api/notes", notesRoutes); // Use the notes routes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => { // Connect to the database then start the server
   app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
-// Create an axios instance with default settings
+
+// in production, there's no localhost so we have to make this dynamic
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
-}); // Default base URL
+  baseURL: BASE_URL,
+});
+
 export default api;
